9f5426f5aaf3e57d450414c559b67776aca0b7ed
PriyankaKediya <prikediya@gmail.com>
2021-10-29 15:26:50 +0530

    Added Add, Edit, Delete buttons and routes, created books database

---
+++ .history/server/config/db_20211029151501.js
@@ -0,0 +1,7 @@
+module.exports = {
+  //local MongoDB deployment ->
+  "LocalURI": "mongodb://localhost/books",
+  "RemoteURI": "mongodb+srv://prikediya:<password>@cluster0.r4ccx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  "HostName": "remoteHost"
+
+};
---
+++ server/config/db.js
@@ -0,0 +1,7 @@
+module.exports = {
+  //local MongoDB deployment ->
+  "LocalURI": "mongodb://localhost/books",
+  "RemoteURI": "mongodb+srv://prikediya:WQRyEFkU3jRQlj1Y@cluster0.r4ccx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  "HostName": "remoteHost"
+
+};
